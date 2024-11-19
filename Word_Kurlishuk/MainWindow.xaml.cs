6661f37d9119b32d61c6a75941ef4c2c494c525a
Hat <cool97438@gmail.com>
2024-11-19 21:05:45 +0500

    Выполненная практическая работа на оценку удовлетворительно. Kurlishuk

--- Word_Kurlishuk/MainWindow.xaml.cs
+++ Word_Kurlishuk/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using Word_Kurlishuk.Context;
 
 namespace Word_Kurlishuk
 {
@@ -20,9 +22,25 @@ namespace Word_Kurlishuk
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow() 
+        public MainWindow()
         {
             InitializeComponent();
+            LoadRooms();
+        }
+
+        public void LoadRooms()
+        {
+            for (int i = 1; i < 20; i++)
+                parent.Children.Add(new Elements.Room(i));
+        }
+
+        private void Report(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Word Files (*.docx)|*.docx";
+            saveFileDialog.ShowDialog();
+            if (saveFileDialog.FileName != "")
+                OwnerContext.Report(saveFileDialog.FileName);
         }
     }
 }
